b9faab3f24dd5223b0e2a28a4805350934a88766
Oleg Polyakov <olegpolyakov@outlook.com>
2016-06-30 13:11:17 +0300

    Angular 2Do v0.1
    Add Semantic UI
    Create app.component.html
    Create app.component.css
    Edit app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,13 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'todo-app',
-    template: '<h1>Angular 2Do</h1>'
+    templateUrl: './app/app.component.html',
+    styleUrls: ['./app/app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {
+    title: string;
+
+    constructor() {
+        this.title = 'Angular 2Do!';
+    }
+}
